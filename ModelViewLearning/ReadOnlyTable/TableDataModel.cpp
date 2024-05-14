fa02b2d71e1e6c799ef1b74a27eb64c04e3787a9
郭翰文 <939162744@qq.com>
2024-05-14 20:45:12 +0800

    Example2 : 使用roles扩展，data中可以安排不同的数据角色

--- ModelViewLearning/ReadOnlyTable/TableDataModel.cpp
+++ ModelViewLearning/ReadOnlyTable/TableDataModel.cpp
@@ -1,4 +1,6 @@
 #include "TableDataModel.h"
+#include <QFont>
+#include <QBrush>
 
 TableDataModel::TableDataModel(QObject* parent /*= nulllpte*/)
     : QAbstractTableModel(parent)
@@ -18,11 +20,61 @@ int TableDataModel::columnCount(const QModelIndex& parent /*= QModelIndex()*/) c
 
 QVariant TableDataModel::data(const QModelIndex& index, int role /*= Qt::DisplayRole*/) const
 {
-    if (Qt::DisplayRole == role)
+    int row = index.row();
+    int col = index.column();
+
+    switch (role)
     {
+    case Qt::DisplayRole:
+    {
+        if (row == 0 && col == 1) return QString("<--left");
+        if (row == 1 && col == 1) return QString("right-->");
+
         return QString("Row%1 , Column%2")
             .arg(index.row() + 1)
             .arg(index.column() + 1);
+        break;
+    }
+    case Qt::FontRole:
+    {
+        // 只改变（0,0）处的字体
+        if (row == 0 && col == 0)
+        {
+            QFont boldFont;
+            boldFont.setBold(true);
+            return boldFont;
+        }
+        break;
+    }
+    case Qt::BackgroundRole:
+    {
+        // 只改变（1,2）处的颜色
+        if (row == 1 && col == 2)  
+        {
+            QBrush redBackground(Qt::red);
+            return redBackground;
+        }
+        break;
+    }
+    case Qt::TextAlignmentRole:
+    {
+        // 改变（1,1）处的对齐格式
+        if (row == 1 && col == 1)
+        {
+            return Qt::AlignRight + Qt::AlignVCenter;
+        }
+        break;
+    }
+    case Qt::CheckStateRole:
+    {
+        if (row == 1 && col == 0)
+        {
+            return Qt::Checked;
+        }
+        break;
+    }
+    default:
+        break;
     }
     return QVariant();
 }
